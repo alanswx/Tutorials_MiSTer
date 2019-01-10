9c0926d936b5edf5d9cf7215181d176fda979e61
Alan Steremberg <alan.steremberg@gmail.com>
2019-01-10 15:04:58 -0800

    First Check In
    This is the first check in of lesson 1 - 3 port from mist to mister

---
+++ lesson1/soc.v
@@ -0,0 +1,31 @@
+// A simple system-on-a-chip (SoC) for the MiST
+// (c) 2015 Till Harbaum
+
+module soc (
+   input  pixel_clock,
+   output SDRAM_nCS,
+   output VGA_HS,
+   output VGA_VS,
+   output [7:0] VGA_R,
+   output [7:0] VGA_G,
+   output [7:0] VGA_B,
+	output VGA_DE
+);
+
+// de-activate unused SDRAM
+assign SDRAM_nCS = 1;
+
+
+vga vga (
+	 .pclk  (pixel_clock),
+	 .hs    (VGA_HS),
+	 .vs    (VGA_VS),
+	 .r     (VGA_R),
+	 .g     (VGA_G),
+	 .b     (VGA_B),
+	 .VGA_DE(VGA_DE)
+);
+					
+
+
+endmodule
